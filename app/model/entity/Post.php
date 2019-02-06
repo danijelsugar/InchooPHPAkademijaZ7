0412a289a96909f9bff1610ec11b4be763364a7d
Danijel Šugar <danijel.sugar@outlook.com>
2019-02-06 17:31:47 +0100

    First commit: added already prepared files

---
+++ app/model/entity/Post.php
@@ -0,0 +1,103 @@
+<?php
+
+class Post
+{
+    private $id;
+
+    private $content;
+
+    private $user;
+
+    private $date;
+
+    private $likes;
+
+    private $comments;
+
+    private $userid;
+
+    public function __construct($id, $content, $user,$date, $likes,$comments,$userid)
+    {
+        $this->setId($id);
+        $this->setContent($content);
+        $this->setUser($user);
+        $this->setDate($date);
+        $this->setLikes($likes);
+        $this->setComments($comments);
+        $this->setUserid($userid);
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return isset($this->$name) ? $this->$name : null;
+    }
+
+    public function __call($name, $arguments)
+    {
+        $function = substr($name, 0, 3);
+        if ($function === 'set') {
+            $this->__set(strtolower(substr($name, 3)), $arguments[0]);
+            return $this;
+        } else if ($function === 'get') {
+            return $this->__get(strtolower(substr($name, 3)));
+        }
+
+        return $this;
+    }
+
+    public static function all()
+    {
+
+        $list = [];
+        $db = Db::connect();
+        $statement = $db->prepare("select 
+        a.id, a.content, concat(b.firstname, ' ', b.lastname) as user, a.date, 
+        count(c.id) as likes
+        from 
+        post a inner join user b on a.user=b.id 
+        left join likes c on a.id=c.post 
+        where a.date > ADDDATE(now(), INTERVAL -7 DAY) 
+        group by a.id, a.content, concat(b.firstname, ' ', b.lastname), a.date 
+        order by a.date desc limit 10");
+        $statement->execute();
+        foreach ($statement->fetchAll() as $post) {
+
+            $statement = $db->prepare("select a.id, a.content, concat(b.firstname, ' ', b.lastname) as user, a.date from comment a inner join user b on a.user=b.id where a.post=:id ");
+            $statement->bindValue('id', $post->id);
+            $statement->execute();
+            $comments = $statement->fetchAll();
+
+            $list[] = new Post($post->id, $post->content, $post->user,$post->date,$post->likes,$comments,0);
+
+        }
+
+        return $list;
+    }
+
+    public static function find($id)
+    {
+        $id = intval($id);
+        $db = Db::connect();
+        $statement = $db->prepare("select 
+        a.id, a.content, concat(b.firstname, ' ', b.lastname) as user, a.date, a.user as userid, count(c.id) as likes
+        from 
+        post a inner join user b on a.user=b.id 
+        left join likes c on a.id=c.post 
+         where a.id=:id");
+        $statement->bindValue('id', $id);
+        $statement->execute();
+        $post = $statement->fetch();
+
+        $statement = $db->prepare("select a.id, a.content, concat(b.firstname, ' ', b.lastname) as user, a.date from comment a inner join user b on a.user=b.id where a.post=:id ");
+        $statement->bindValue('id', $id);
+        $statement->execute();
+        $comments = $statement->fetchAll();
+
+        return new Post($post->id, $post->content, $post->user, $post->date,$post->likes, $comments,$post->userid);
+    }
+}
---
+++ script.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS social_network;
+CREATE DATABASE social_network CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+use social_network;
+
+create table user(
+id int not null primary key auto_increment,
+firstname varchar(50) not null,
+lastname varchar(50) not null,
+email varchar(100) not null,
+pass char(60) not null
+)engine=InnoDB;
+
+create unique index ix1 on user(email);
+
+
+create table post(
+id int not null primary key auto_increment,
+content text,
+user int not null,
+date datetime not null default now()
+)engine=InnoDB;
+
+create table comment(
+id int not null primary key auto_increment,
+user int not null,
+post int not null,
+content text not null,
+date datetime not null default now()
+)engine=InnoDB;
+
+create table likes(
+id int not null primary key auto_increment,
+user int not null,
+post int not null
+)engine=InnoDB;
+
+
+alter table post add FOREIGN KEY (user) REFERENCES user(id);
+
+alter table comment add FOREIGN KEY (user) REFERENCES user(id);
+alter table comment add FOREIGN KEY (post) REFERENCES post(id);
+
+alter table likes add FOREIGN KEY (user) REFERENCES user(id);
+alter table likes add FOREIGN KEY (post) REFERENCES post(id);
+
+
+insert into user (id,firstname,lastname,email,pass) values
+(null,'Pero','Perić','pperic@gmail.com','$2y$10$9.r4TBJ3Yd7UeuofhqdJ1uIDc6Y4kAD2QKQN1PYcuIlK3uliASY9m');
+
+insert into user (firstname,lastname,email,pass) values
+('Ana','Ančić','aancic@gmail.com','$2y$10$9.r4TBJ3Yd7UeuofhqdJ1uIDc6Y4kAD2QKQN1PYcuIlK3uliASY9m');
+
+
+insert into post (content,user) values ('Evo danas pada kiša opet :(',1), ('Jedem jagode.',2);
+
